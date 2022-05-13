4bdcf472e5eeaf0d61b0e016c0b806c179c67361
mchibane <mchibane@student.42.fr>
2022-05-13 16:09:44 +0200

    added make_pair function in pair.hpp

--- pair.hpp
+++ pair.hpp
@@ -66,6 +66,14 @@ namespace ft
 	template<class T1, class T2>
 	bool	operator>=(pair<T1, T2> const &lhs, pair<T1, T2> const &rhs) { return (!(lhs < rhs)); }
 
+
+		/* MAKE PAIR FUNCTION */
+
+	template<class T1, class T2>
+	pair<T1, T2>	make_pair(T1 x, T2 y)
+	{
+		return (pair<T1, T2>(x, y));
+	}
 };
 
 #endif
