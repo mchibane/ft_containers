2d1001aef167fa1a1c1414eb54951de053f2bdd1
mchibane <mchibane@student.42.fr>
2022-05-16 20:32:07 +0200

    first try on vector iterators, need to fix some things up though

--- vectorIterator.hpp
+++ vectorIterator.hpp
@@ -24,13 +24,20 @@ namespace ft
 
 		vectorIterator(void) : _ptr(NULL) {}
 		vectorIterator(vectorIterator const &x) : _ptr(x._ptr) {}
+		vectorIterator(pointer const &ptr) : _ptr(ptr) {}
 		vectorIterator	&operator=(vectorIterator const &rhs)
 		{
-			_ptr = rhs._ptr;
+			if (this != &rhs)
+				_ptr = rhs._ptr;
 			return (*this);
 		}
 		~vectorIterator(void) {}
 
+		template<typename I>
+		operator vectorIterator<const I>(void)
+		{
+			return (vectorIterator<const I>(_ptr));
+		}
 
 			/* EQUIVALENCES */
 
@@ -42,16 +49,92 @@ namespace ft
 
 			/* DEREFERENCE */
 
-		reference	operator*(void) const	{return (*_ptr); }
+		reference			operator*(void) const	{return (*_ptr); }
 		vectorIterator		operator->(void) const	{return (_ptr); }
 
 
-			/* INCREMENTATION */
+			/* INCREMENTATION & DECREMENTATION */
 
 		vectorIterator	&operator++(void)
 		{
 			_ptr++;
 			return (*this);
+		}
+		vectorIterator	operator++(int)
+		{
+			return (vectorIterator(_ptr++));
+		}
+
+		vectorIterator	&operator--(void)
+		{
+			_ptr--;
+			return (*this);
+		}
+		vectorIterator	operator--(int)
+		{
+			return (vectorIterator(_ptr--));
+		}
+
+
+			/* ARITHMETIC */
+
+		vectorIterator	operator+(difference_type n) const
+		{
+			return (vectorIterator(_ptr + n));
+		}
+
+		vectorIterator	operator-(difference_type n) const
+		{
+			return (vectorIterator(_ptr - n));
+		}
+		difference_type	operator-(vectorIterator const &x) const
+		{
+			return (_ptr - x._ptr);
+		}
+
+
+			/* INEQUALITY */
+
+		template<typename I>
+		bool	operator<(vectorIterator<I> const &rhs)
+		{
+			return (_ptr < rhs._ptr);
+		}
+		template<typename I>
+		bool	operator>(vectorIterator<I> const &rhs)
+		{
+			return (rhs < *this);
+		}
+		template<typename I>
+		bool	operator<=(vectorIterator<I> const &rhs)
+		{
+			return (!(rhs < *this));
+		}
+		template<typename I>
+		bool	operator>=(vectorIterator<I> const &rhs)
+		{
+			return (!(*this < rhs));
+		}
+
+
+			/* COMPOUND ASSIGNMENT */
+
+		vectorIterator &operator+=(difference_type const &n)
+		{
+			_ptr += n;
+			return (*this);
+		}
+
+		vectorIterator	&operator-=(difference_type const &n)
+		{
+			_ptr -= n;
+			return (*this);
+		}
+
+
+		reference	operator[](difference_type const &n) const
+		{
+			return (_ptr[n]);
 		}
 	};
 };
