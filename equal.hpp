d229e319c02e596b2548cbcd619a876100fb7b5c
mchibane <mchibane@student.42.fr>
2022-05-16 17:36:26 +0200

    added namespace ft{} for equal.hpp and lexicographicalCompare.hpp

--- equal.hpp
+++ equal.hpp
@@ -1,30 +1,33 @@
 #ifndef EQUAL_HPP
 #define EQUAL_HPP
 
-template<class InputIterator1, class InputIterator2>
-bool	equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+namespace ft
 {
-	while (first1 != last1)
+	template<class InputIterator1, class InputIterator2>
+	bool	equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
 	{
-		if (!(*first1 == *first2))
-			return (false);
-		++first1;
-		++first2;
+		while (first1 != last1)
+		{
+			if (!(*first1 == *first2))
+				return (false);
+			++first1;
+			++first2;
+		}
+		return (true);
 	}
-	return (true);
-}
 
-template<class InputIterator1, class InputIterator2, class BinaryPredicate>
-bool	equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred)
-{
-	while (first1 != last1)
+	template<class InputIterator1, class InputIterator2, class BinaryPredicate>
+	bool	equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred)
 	{
-		if (!pred(*first1, *first2))
-			return (false);
-		++first1;
-		++first2;
+		while (first1 != last1)
+		{
+			if (!pred(*first1, *first2))
+				return (false);
+			++first1;
+			++first2;
+		}
+		return (true);
 	}
-	return (true);
-}
+};
 
 #endif
