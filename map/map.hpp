ab71870ddd6de68ca7304202f11d3d0cc4746d6b
mchibane <mchibane@student.42.fr>
2022-05-17 21:30:59 +0200

    added simple class proto for map

---
+++ map/map.hpp
@@ -0,0 +1,23 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include <functional>
+#include "../pair.hpp"
+
+#include <iostream>
+
+namespace ft
+{
+	template< class Key, class T, class Compare = std::less<Key>, class Alloc = std::allocator< ft::pair<const Key, T> > >
+	class map
+	{
+		public:
+
+		private:
+
+		public:
+
+	}; // class map
+}; //namespace ft
+
+#endif
